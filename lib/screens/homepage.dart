2b61933b9811c24d43b3939380ff7588ed9f54a3
Samarth Asthan <97031010+SamarthAsthan@users.noreply.github.com>
2022-09-03 02:20:58 +0530

    Pageview Added
    Vertical Pageview added with some static news data

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, sort_child_properties_last, avoid_unnecessary_containers, sized_box_for_whitespace
 
 import 'package:flutter/material.dart';
 
@@ -7,19 +7,70 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text(
-            "TheShorts",
-            style: TextStyle(color: Colors.black),
+    //Controller for managing pages.
+    final _controller = PageController();
+
+    //Page List
+    List<Widget> news = [
+      pagess(),
+      pagess(),
+      pagess(),
+      pagess(),
+      pagess(),
+      pagess(),
+      pagess(),
+      pagess(),
+      pagess(),
+    ];
+
+    return Container(
+      color: Colors.black,
+      child: SafeArea(
+        child: Scaffold(
+          body: Container(
+            child: PageView(
+              children: news,
+              controller: _controller,
+              scrollDirection: Axis.vertical,
+            ),
           ),
-          backgroundColor: Colors.white,
         ),
-        body: Center(
-          child: Text(
-            "Welcome",
-            style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+}
+
+//Cutom Widget
+class pagess extends StatelessWidget {
+  const pagess({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height,
+      width: MediaQuery.of(context).size.width,
+      child: Column(
+        children: [
+          Image.network(
+            'https://picsum.photos/1280/720?image=16',
+            alignment: Alignment.topCenter,
           ),
-        ));
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              "Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.",
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: Text(
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+              style: TextStyle(fontSize: 16),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
